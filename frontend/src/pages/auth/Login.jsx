9443321bff28aaba4a8a4775e588c5a2e1d2902f
aguerima <imanolaguer1@gmail.com>
2024-11-30 01:41:29 -0300

    Frontend: Auth Funcionality and Pages -> Register & Login

---
+++ frontend/src/pages/auth/Login.jsx
@@ -0,0 +1,115 @@
+import React, { useState } from 'react'
+import { UserOutlined, LockOutlined } from '@ant-design/icons'
+import { Button, Checkbox, Form, Input, Flex } from 'antd'
+// import { logoHero } from '../../../public/img/taskmanager'
+
+// import { loginUser } from '../../services/api/users'
+import { useNavigate, Link } from 'react-router-dom'
+import { useUserContext } from '../../contexts/userContext'
+
+
+
+export function Login() {
+    const { loginUser } = useUserContext()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        localStorage.removeItem('authToken')
+    }, [])
+
+    const onFinish = (values) => {
+        console.log('Reveiced values of form: ', values)
+    }
+
+    const [loginUserData, setLoginUserData] = useState({
+        email: '',
+        password: '',
+    }
+    )
+
+    const handleLoginSubmit = async (e) => {
+        e.preventDefault()
+        console.log('Handling Login Submit')
+
+        const payload = {
+            email: loginUserData.email,
+            password: loginUserData.password,
+        }
+
+        console.log(payload)
+
+        const successLogin = await loginUser(payload)
+
+        if (successLogin) {
+            const redirectTo = '/'
+            console.log(successLogin)
+
+            localStorage.setItem('authToken', successLogin.data.token)
+            localStorage.setItem('authPass', loginUserData.password)
+            localStorage.setItem('authRole', (successLogin.data.user.role)) // cuidado al parsear mal con JSON.stringify o JSON.parse
+
+            navigate(redirectTo, { replace: true })
+        }
+    }
+
+    return (
+        <div className="flex-col justify-center w-full h-full place-items-center"
+        >
+            <img src='/img/taskmanager.png' alt="" className="my-24" />
+
+            <div className="bg-blue-200 shadow-md shadow-blue-300 rounded-xl w-[700px] h-[400px] flex justify-center place-items-center">
+                <Form className='w-[400px]'
+                    name="login"
+                    initialValues={{
+                        remember: true,
+                    }}
+                    style={{
+                        maxWidth: 360,
+                    }}
+                    onFinish={onFinish}
+                >
+
+                    <h1 className="text-[20px] mb-8 font-bold text-center">Login with your account</h1>
+
+                    <Form.Item
+                        name="email"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Email!',
+                            },
+                        ]}
+                    >
+                        <Input prefix={<UserOutlined />} placeholder="Email" onChange={(e) => setLoginUserData({ ...loginUserData, email: e.target.value })} />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Password!',
+                            },
+                        ]}
+                    >
+                        <Input prefix={<LockOutlined />} type="password" placeholder="Password" onChange={(e) => setLoginUserData({ ...loginUserData, password: e.target.value })} />
+                    </Form.Item>
+                    <Form.Item>
+                        <Flex justify="space-between" align="center">
+                            <Form.Item name="remember" valuePropName="checked" noStyle>
+                                <Checkbox>Remember me</Checkbox>
+                            </Form.Item>
+                            <a href="">Forgot password</a>
+                        </Flex>
+                    </Form.Item>
+
+                    <Form.Item>
+                        <Button block type="primary" htmlType="submit" onClick={handleLoginSubmit}>
+                            Log in
+                        </Button>
+                        or <Link to="/register">Register now!</Link>
+                    </Form.Item>
+                </Form>
+            </div>
+        </div>
+    )
+}
---
+++ frontend/src/pages/auth/Register.jsx
@@ -0,0 +1,126 @@
+import { Form, Input, Button, Checkbox } from 'antd'
+import React, { useState } from 'react'
+import { useUserContext } from '../../contexts/userContext'
+import { useNavigate } from 'react-router-dom'
+
+
+const onFinish = (values) => {
+    console.log('Success:', values);
+};
+const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+};
+
+
+export function Register() {
+
+    const { createUser } = useUserContext()
+
+    const [newUser, setNewUser] = useState({
+        name: "",
+        email: "",
+        password: "",
+    })
+
+    const navigate = useNavigate()
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const payload = {
+            name: newUser.name,
+            email: newUser.email,
+            password: newUser.password,
+        }
+
+        const successRegistration = await createUser(payload)
+
+        if (successRegistration) {
+            const redirectTo = '/'
+            console.log('Redireccionando')
+
+            navigate(redirectTo, { replace: true })
+        }
+    }
+
+    console.log(newUser)
+
+
+    return (
+        <div className="flex-col justify-center h-full place-items-center">
+
+
+            <img src='/img/taskmanager.png' alt="" className="my-24" />
+
+            <div className="shadow-sm shadow-black h-96 w-[700px] flex flex-col justify-center place-items-center rounded-xl">
+
+                <Form className=""
+                    name="basic"
+                    labelCol={{
+                        span: 8,
+                    }}
+                    wrapperCol={{
+                        span: 16,
+                    }}
+                    style={{
+                        maxWidth: 600,
+                    }}
+                    initialValues={{
+                        remember: true,
+                    }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+                >
+
+                    <h1 className="text-[20px] mb-8 font-bold text-center">Register your account</h1>
+
+                    <Form.Item
+                        label="Username"
+                        name="username"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your username!',
+                            },
+                        ]}
+                    >
+                        <Input className="" onChange={(e) => setNewUser({ ...newUser, name: e.target.value })} />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Email"
+                        name="email"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your email!',
+                            },
+                        ]}
+                    >
+                        <Input className="w-[100%]" onChange={(e) => setNewUser({ ...newUser, email: e.target.value })} />
+                    </Form.Item>
+
+
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your password!',
+                            },
+                        ]}
+                    >
+                        <Input.Password className="" onChange={(e) => setNewUser({ ...newUser, password: e.target.value })} />
+                    </Form.Item>
+
+                    <Form.Item label={null}>
+                        <button className='text-[18px] w-24 h-10 bg-green-400 rounded hover:bg-green-700' onClick={handleSubmit}>Register</button>
+                    </Form.Item>
+                </Form>
+            </div>
+        </div>
+    )
+}
